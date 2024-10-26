2b57952728edc7ae3ccdb897660234645f8e7fac
Yash <yashchaurasia630@gmail.com>
2024-10-26 14:18:10 +0530

    menger sponge

--- 02-Menger sponge/main.c
+++ 02-Menger sponge/main.c
@@ -33,12 +33,16 @@ void DrawMengerBase(int Ox, int Oy, int Oz) {
 
 void DrawMenger(int level, int Ox, int Oy, int Oz) {
   int offset = pow(3, level - 1) * SIDE;
+  int midHole = pow(3, level - 1);
   if (level > 1) {
     for (int x = 0; x < edge; x++) {
       for (int y = 0; y < edge; y++) {
         for (int z = 0; z < edge; z++) {
-          DrawMenger(level - 1, Ox + x * offset, Oy + y * offset,
-                     Oz + z * offset);
+          if ((x == 1 && y == 1) || (z == 1 && (y == 1 || x == 1)))
+            continue;
+          else
+            DrawMenger(level - 1, Ox + x * offset, Oy + y * offset,
+                       Oz + z * offset);
         }
       }
     }
@@ -51,20 +55,20 @@ int main(void) {
   int winw = 800;
   int winh = 600;
   int level = 1;
+  int offset = pow(3, level) * SIDE;
 
   InitWindow(winw, winh, "Menger cube");
   SetExitKey(KEY_Q);
   SetTargetFPS(60);
 
   Camera3D camera = {0};
+  camera.position = (Vector3){2 * offset, 2 * offset, 2 * offset};
   camera.target = (Vector3){0.0f, 0.0f, 0.0f};
   camera.up = (Vector3){0.0f, 0.1f, 0.0f};
   camera.fovy = 45.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
   while (!WindowShouldClose()) {
-    int offset = pow(3, level) * SIDE;
-    camera.position = (Vector3){offset + 100, offset + 100, offset + 100};
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsKeyDown(KEY_W) ||
         IsKeyDown(KEY_S) || IsKeyDown(KEY_A) || IsKeyDown(KEY_D) ||
         IsKeyDown(KEY_Q) || IsKeyDown(KEY_E))
@@ -72,6 +76,8 @@ int main(void) {
 
     if (IsKeyPressed('Z')) camera.target = (Vector3){0.0f, 0.0f, 0.0f};
     if (IsKeyPressed(KEY_ENTER)) {
+      offset = pow(3, level) * SIDE;
+      camera.position = (Vector3){2 * offset, 2 * offset, 2 * offset};
       level++;
       TraceLog(LOG_INFO, "level = %d", level);
     }
