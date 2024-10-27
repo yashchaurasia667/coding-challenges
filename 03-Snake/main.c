bb06cd01d20902ec7c3d44555f158188829f1a1c
Yash <yashchaurasia630@gmail.com>
2024-10-27 13:35:09 +0530

    openGL setup with visual studio 2022

--- 03-Snake/main.c
+++ 03-Snake/main.c
@@ -1,13 +1,36 @@
 #include <raylib.h>
 #include <stdio.h>
 
+struct segment {
+  int x;
+  int y;
+};
+
+typedef struct segment Segment;
+
+void DrawGrid2D(int spacing, Color color) {
+  int winW = GetScreenWidth();
+  int winH = GetScreenHeight();
+
+  for (int x = 0; x < winW; x += spacing) {
+    DrawLine(x, 0, x, winH, color);
+  }
+
+  for (int y = 0; y < winH; y += spacing) {
+    DrawLine(0, y, winW, y, color);
+  }
+}
+
 int main() {
   int winw = 800;
   int winh = 600;
+
   InitWindow(winw, winh, "Snake");
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
+    DrawGrid2D(50, WHITE);
     EndDrawing();
   }
 
