d005ca8a0c88a38284720f428e09d37adc27ea79
Yash <yashchaurasia630@gmail.com>
2024-11-07 12:14:56 +0000

    movement

--- 03-Snake/src/main.cpp
+++ 03-Snake/src/main.cpp
@@ -3,7 +3,7 @@
 #include "snake.h"
 #include <raylib.h>
 
-#define FPS 10
+#define FPS 5
 #define SPEED 40
 
 void draw(std::vector<snake> body)
@@ -22,6 +22,40 @@ std::vector<snake> update(std::vector<snake> body)
   return body;
 }
 
+Vector2 renderFood(int width, int height)
+{
+  return (Vector2){
+      rand() % width,
+      rand() % height,
+  };
+}
+
+snake handleInput(key_t key, snake head)
+{
+  if (key == KEY_UP && head.speedY != 1)
+  {
+    head.speedY = -1;
+    head.speedX = 0;
+  }
+  if (key == KEY_DOWN && head.speedY != -1)
+  {
+    head.speedY = 1;
+    head.speedX = 0;
+  }
+  if (key == KEY_LEFT && head.speedX != 1)
+  {
+    head.speedX = -1;
+    head.speedY = 0;
+  }
+  if (key == KEY_RIGHT && head.speedX != -1)
+  {
+    head.speedX = 1;
+    head.speedY = 0;
+  }
+
+  return head;
+}
+
 int main()
 {
   int winw = 800, winh = 600;
@@ -29,9 +63,13 @@ int main()
   SetExitKey(KEY_Q);
   SetTargetFPS(FPS);
 
+  srand(time(0));
+
   std::vector<snake> body;
   snake *segment = new snake((Vector2){100, 100});
   body.push_back(*segment);
+
+  Vector2 foodPos = renderFood(winw, winh);
 
   while (!WindowShouldClose())
   {
@@ -41,10 +79,18 @@ int main()
       winh = GetScreenHeight();
     }
 
+    body[0] = handleInput(GetKeyPressed(), body[0]);
+
     BeginDrawing();
     ClearBackground(BLACK);
+
+    if (foodPos.x == NULL)
+      foodPos = renderFood(winw, winh);
+    DrawRectangle(foodPos.x, foodPos.y, 30, 30, RED);
+
     draw(body);
     body = update(body);
+
     EndDrawing();
   }
 
